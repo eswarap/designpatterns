87620eda0b949e417122d44cbbe724e7eb477240
eswara <eswaraprasadh@yahoo.com>
2017-02-24 11:37:42 +0000

    test coverage added

--- src/test/java/org/woven/example/servicelocator/ServiceLocatorTest.java
+++ src/test/java/org/woven/example/servicelocator/ServiceLocatorTest.java
@@ -4,8 +4,6 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.woven.examples.servicelocator.Service;
 import org.woven.examples.servicelocator.ServiceLocator;
 import org.woven.examples.servicelocator.ServiceType;
@@ -13,17 +11,15 @@ import org.woven.examples.servicelocator.ServiceType;
 @RunWith(JUnit4.class)
 public class ServiceLocatorTest {
     
-    private final static Logger LOG = LoggerFactory.getLogger(ServiceLocatorTest.class);
-     
     @Test
     public void testServiceLocator() {
         ServiceLocator serviceLocator = new ServiceLocator();
         
         Service dbService = serviceLocator.getService(ServiceType.DatabaseService);
-        LOG.info(dbService.getServiceType().toString());
+        dbService.execute();
         Assert.assertEquals(dbService.getServiceType(), ServiceType.DatabaseService);
         Service msgService = serviceLocator.getService(ServiceType.MessagingService);
-        LOG.info(msgService.getServiceType().toString());
+        msgService.execute();
         Assert.assertEquals(msgService.getServiceType(), ServiceType.MessagingService);
     }
 }
